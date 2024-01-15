47f701598f1480731d1005bf1393e69ffb0a75c0
MahammedJafarzade <7a4m9bb@code.edu.az>
2024-01-15 23:58:28 +0400

    login with facebook and google usage of firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,16 +79,19 @@ android {
 }
 
 dependencies {
+
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+
     implementation("com.facebook.android:facebook-login:latest.release")
     implementation("com.facebook.android:facebook-android-sdk:15.1.0")
 
-
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
 
     implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-auth")
-
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
--- app/src/main/java/com/mahammadjafarzade/authenticationfirebase/features/login/LoginFragment.kt
+++ app/src/main/java/com/mahammadjafarzade/authenticationfirebase/features/login/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.facebook.AccessToken
 import com.facebook.CallbackManager
@@ -15,6 +16,11 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseError
 import com.google.firebase.auth.FacebookAuthProvider
@@ -22,6 +28,7 @@ import com.google.firebase.auth.FirebaseAuthActionCodeException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.auth
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.mahammadjafarzade.authenticationfirebase.R
 import com.mahammadjafarzade.authenticationfirebase.databinding.FragmentLoginBinding
 import com.mahammadjafarzade.authenticationfirebase.databinding.FragmentRegisterBinding
@@ -49,6 +56,9 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate(inflater)
         binding.loginButtonfb.setOnClickListener {
             loginFacebook()
+        }
+        binding.loginButtonGoogle.setOnClickListener {
+            loginGoogle()
         }
         binding.loginButton.setOnClickListener{
             login()
@@ -135,6 +145,62 @@ class LoginFragment : Fragment() {
                 }
             }
     }
+    //Google
+    private lateinit var signInRequest: BeginSignInRequest
+
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+    fun loginGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestServerAuthCode(getString(R.string.google_client_id))
+            .requestIdToken(getString(R.string.google_client_id))
+            .requestEmail().build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.google_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
+
+        val signInIntent = mGoogleSignInClient.signInIntent
+        googleLauncher.launch(signInIntent)
+    }
+
+    private val googleLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: androidx.activity.result.ActivityResult ->
+
+        result.data?.let {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(it)
+            if(result?.isSuccess == true) {
+                val acct = result.signInAccount
+                val authCode = acct!!.serverAuthCode
+                val idToken = acct.idToken
+
+                val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                Firebase.auth.signInWithCredential(firebaseCredential)
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success")
+                            val user = auth.currentUser
+                            openMain()
+                            //updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.exception)
+                            //updateUI(null)
+                        }
+                    }
+            }
+        }
+    }
+
     fun openApp(){
         val action = LoginFragmentDirections.actionLoginFragmentToNotesFragment()
         findNavController().navigate(action)
